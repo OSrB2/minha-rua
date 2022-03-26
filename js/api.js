09f9943c11b6ac72f143c3269f4d37de8ce3133c
Pedro Oliveira <pedro_oliva@outlook.com.br>
2022-03-26 11:20:13 -0300

    requisição api

--- js/api.js
+++ js/api.js
@@ -1,9 +1,17 @@
 const inputCep = document.querySelector('#cep');
+const BASE_URL = 'https://brasilapi.com.br/api';
 
-inputCep.onkeyup = (evento) => {
+inputCep.onkeyup = async (evento) => {
+  // Verificando se o cep cumpriu os 8 digitos.
   if (inputCep.value.length < 8) {
     return;
   }
 
+  // Fazendo uma requisição a api BRASILAPI para buscar as informações com o cep digitado.
+  const resposta = await fetch(`${BASE_URL}/cep/v1/${inputCep.value}`, {
+    method: 'GET',
+  });
+
+  console.log(resposta);
   alert('CEP completo!');
 };
